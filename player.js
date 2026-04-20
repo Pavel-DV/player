a3773c5b7cbd798951bd463ad9313f2d8e10374d
Your Name <you@example.com>
2026-04-20 11:45:06 -0400

    color green

--- player.js
+++ player.js
@@ -33,7 +33,7 @@ import { createUiController } from './player/ui.js';
 
 const dom = getPlayerDom();
 const state = createPlayerState();
-window.__playerBuildId = '63';
+window.__playerBuildId = '67';
 console.log('Player build:', window.__playerBuildId);
 const { playlists, currentPlaylistId } = loadPlaylists();
 
