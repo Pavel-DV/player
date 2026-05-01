228e6587b436822de6ef5eb6e6b888bd8c88df26
Your Name <you@example.com>
2026-05-01 14:32:57 -0400

    the refresh button is moved

--- player.js
+++ player.js
@@ -43,7 +43,7 @@ import { createUiController } from './player/ui.js';
 
 const dom = getPlayerDom();
 const state = createPlayerState();
-window.__playerBuildId = '122';
+window.__playerBuildId = '123';
 console.log('Player build:', window.__playerBuildId);
 const buildVersionEl = document.getElementById('buildVersion');
 
